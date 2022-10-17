9831bfb0261e506ea8cbd7c8371052919680d295
Tafaha <s351891@oslomet.no>
2022-10-17 21:12:01 +0200

    Add project files.

---
+++ Stock-trading/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stock_trading.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
