8d873b6753d3b455509a6a93d14258076f42577c
Imad Fayad <imad.fayad.if@gmail.com>
2022-10-24 17:08:39 +0200

    Displaying price inside the input

---
+++ Stock-trading/Migrations/20221024132351_AddAksjeToDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock_trading.Migrations
+{
+    public partial class AddAksjeToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aksje",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Navn = table.Column<int>(nullable: false),
+                    Pris = table.Column<double>(nullable: false),
+                    Antall = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aksje", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aksje");
+        }
+    }
+}
