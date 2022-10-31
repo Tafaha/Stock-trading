4eceeff7df1c7b2ba7ff86a6b9c922027b294104
marko <markospyros1@gmail.com>
2022-10-31 11:27:02 +0100

    Endret på database stringen

---
+++ Stock-trading/Migrations/20221030223459_addAksjeToDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stock_trading.Migrations
+{
+    public partial class addAksjeToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aksjer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Navn = table.Column<int>(nullable: false),
+                    Pris = table.Column<double>(nullable: false),
+                    Antall = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aksjer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aksjer");
+        }
+    }
+}
